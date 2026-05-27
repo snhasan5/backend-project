2216175490cbd2ddceb540e5d570cd37b6125c8e
snhasan5 <163376721+snhasan5@users.noreply.github.com>
2026-05-27 12:39:20 +0530

    utils (apires,err aysnchandler .. etc)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 // require('dotenv').config({path:'../'})
 import dotenv from "dotenv";
+import { app } from "./app.js";
 dotenv.config({
     path:'./.env'
 })
 import express from "express";
 import connectDB from "./db/db.js";
+import { error, log } from "console";
 
-connectDB();
+connectDB().then(()=>{
+  app.listen(process.env.PORT,()=>{
+            console.log(`App is running on port : ${process.env.PORT}`)
+        })
+}).catch((error)=>{
+    console.log("MONGODB connection failed !!! ",error);
+    
+});
 /* Approch 1
 const app = express();
 ;(async ()=>{
